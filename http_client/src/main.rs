91eda167f77406342c45c5c0aa164c2b7a9ad55e
nordluma <maxnordlund.95@hotmail.com>
2024-04-08 21:41:34 +0300

    feat(http_client): forward stream to `upload_server`

--- http_client/src/main.rs
+++ http_client/src/main.rs
@@ -1,11 +1,14 @@
 use bytes::Bytes;
 use futures_util::Stream;
-use reqwest::Client;
+use reqwest::{Body, Client};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = Client::new();
-    let _file = request_file(client, "https://wallpapercave.com/wp/wp5111087.jpg").await?;
+    let file = request_file(&client, "https://wallpapercave.com/wp/wp5111087.jpg").await?;
+    let response = upload_file(&client, file).await?;
+
+    println!("{}", response);
 
     Ok(())
 }
@@ -17,7 +20,7 @@ struct FileStream<S> {
 }
 
 async fn request_file(
-    client: Client,
+    client: &Client,
     url: &str,
 ) -> reqwest::Result<FileStream<impl Stream<Item = reqwest::Result<Bytes>>>> {
     let response = client.get(url).send().await?;
@@ -33,4 +36,23 @@ async fn request_file(
             .expect("response should have a content_length"),
         stream: response.bytes_stream(),
     })
+}
+
+async fn upload_file<S>(client: &Client, file_stream: FileStream<S>) -> reqwest::Result<String>
+where
+    S: Stream<Item = reqwest::Result<Bytes>> + Send + Sync + 'static,
+{
+    let body_stream = Body::wrap_stream(file_stream.stream);
+    let file_part =
+        reqwest::multipart::Part::stream_with_length(body_stream, file_stream.content_length)
+            .file_name(file_stream.filename);
+    let multipart_form = reqwest::multipart::Form::new().part("file", file_part);
+
+    client
+        .post("http://127.0.0.1:8080/")
+        .multipart(multipart_form)
+        .send()
+        .await?
+        .text()
+        .await
 }
